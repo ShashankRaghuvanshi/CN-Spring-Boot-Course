1fd2eca450cd492e09c3ed3088a7fa0f1135910e
Shashank Raghuvanshi <ishashankraghuvanshi@gmail.com>
2025-06-24 12:07:52 +0530

    Delete EventRegistration II directory

--- EventRegistration II/src/main/java/com/example/EventRegistration/Attendee.java
+++
@@ -1,8 +0,0 @@
-package com.example.EventRegistration;
-
-public interface Attendee {
-
-    void setAttendeeDetails(String name, String department, int batch);
-    void printRegistrationConfirmation();
-    String getAttendeeName();
-}
--- EventRegistration II/src/main/java/com/example/EventRegistration/College.java
+++
@@ -1,7 +0,0 @@
-package com.example.EventRegistration;
-
-public interface College {
-
-    String getCollegeName();
-    CollegeEvent getEvent();
-}
