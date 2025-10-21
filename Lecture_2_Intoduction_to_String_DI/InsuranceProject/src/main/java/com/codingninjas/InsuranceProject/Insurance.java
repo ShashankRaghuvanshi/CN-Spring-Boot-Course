3048d770547a2e190b145847d1a4e5ef6ade2dc4
Shashank Raghuvanshi <ishashankraghuvanshi@gmail.com>
2025-10-21 08:45:07 +0530

    Delete Lecture_2_Intoduction_to_String_DI/InsuranceProject directory

--- Lecture_2_Intoduction_to_String_DI/InsuranceProject/src/main/java/com/codingninjas/InsuranceProject/Insurance.java
+++
@@ -1,8 +0,0 @@
-package com.codingninjas.InsuranceProject;
-
-public interface Insurance {
-	public double getInsurancePremium();
-	public String getName(); 
-
-	public void setInsuranceDetails(boolean m1, boolean m2, boolean m3);
-}
