7e8d975b739a0860c21d3407a9b0cae2484eee51
Shashank Raghuvanshi <ishashankraghuvanshi@gmail.com>
2026-01-07 11:08:14 +0530

    Add files via upload

---
+++ Lecture_13_Test_E_Voting_Application/E_voting_System_Application/src/main/java/com/codingninjas/EVotingSystem/repositories/VoteRepository.java
@@ -0,0 +1,20 @@
+package com.codingninjas.EVotingSystem.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.codingninjas.EVotingSystem.entities.Election;
+import com.codingninjas.EVotingSystem.entities.Vote;
+
+public interface VoteRepository extends JpaRepository<Vote, Long> {
+
+	@Query(value = "SELECT COUNT(*) FROM vote", nativeQuery = true)
+	Long countTotalVotes();
+
+	@Query("SELECT COUNT(v) FROM Vote v WHERE v.election.name = :election")
+	Long countVotesByElectionName(@Param("election") String electionName);
+
+	boolean existsByUserIdAndElectionId(Long userId, Long electionId);
+
+}
